820468082e2b8d8ad364cc97b3541eb3fb4c6d15
Carlos Felipe <cfeelipes@gmail.com>
2018-08-22 00:52:23 -0300

    Initial commit

---
+++ TesteWebApi/Controllers/TesteController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using TesteWebApi.Models;
+
+namespace TesteWebApi.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class TesteController : ApiController
+    {
+        // GET: api/Teste
+        public IEnumerable<Aluno> Get()
+        {
+            Aluno aluno = new Aluno();
+            return aluno.ListarAluno();
+        }
+
+        // GET: api/Teste/5
+        public Aluno Get(int id)
+        {
+            Aluno aluno = new Aluno();
+            return aluno.ListarAluno().Where(Aluno => Aluno.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Teste
+        public List<Aluno> Post(Aluno aluno)
+        {
+            Aluno _aluno = new Aluno();
+            _aluno.Inserir(aluno);
+            return _aluno.ListarAluno();
+        }
+
+        // PUT: api/Teste/5
+        public List<Aluno> Put(int id, [FromBody]Aluno aluno)
+        {
+            Aluno _aluno = new Aluno();
+            _aluno.Atualizar(id, aluno);
+            return _aluno.ListarAluno();
+        }
+
+        // DELETE: api/Teste/5
+        public List<Aluno> Delete(int id)
+        {
+            Aluno _aluno = new Aluno();
+            _aluno.Deletar(id);
+            return _aluno.ListarAluno();
+        }
+    }
+}
---
+++ TesteWebApi/Models/Aluno.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace TesteWebApi.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Telefone{ get; set; }
+        public int Ra { get; set; }
+
+        public List<Aluno> ListarAluno()
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+            var json = File.ReadAllText(caminhoArquivo);
+            var listaAlunos = JsonConvert.DeserializeObject<List<Aluno>>(json);
+
+            return listaAlunos;
+        }
+
+        public bool ReescreverArquivo(List<Aluno> listaAlunos)
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+            var json = JsonConvert.SerializeObject(listaAlunos, Formatting.Indented);
+            File.WriteAllText(caminhoArquivo, json);
+
+            return true;
+        }
+
+        public Aluno Inserir(Aluno Aluno)
+        {
+            var listaAlunos = this.ListarAluno();
+            var maxId = listaAlunos.Max(aluno => aluno.Id);
+            Aluno.Id = maxId + 1;
+            listaAlunos.Add(Aluno);
+
+            ReescreverArquivo(listaAlunos);
+            return Aluno;
+        }
+
+        public Aluno Atualizar(int id, Aluno aluno)
+        {
+            var listaAlunos = this.ListarAluno();
+
+            var itemIndex = listaAlunos.FindIndex(Aluno => Aluno.Id == id);
+            if (itemIndex >= 0)
+            {
+                aluno.Id = id;
+                listaAlunos[itemIndex] = aluno;
+            }
+            else
+            {
+                return null;
+            }
+
+            ReescreverArquivo(listaAlunos);
+            return aluno;
+        }
+
+        public bool Deletar(int id)
+        {
+            var listaAlunos = this.ListarAluno();
+            //lambda: procure na lista e retorne o id que seja igual ao id passado no parâmetro
+            var itemIndex = listaAlunos.FindIndex(Aluno => Aluno.Id == id);
+
+            if (itemIndex >= 0)
+            {
+                listaAlunos.RemoveAt(itemIndex);
+            }
+            else
+            {
+                return false;
+            }
+
+            ReescreverArquivo(listaAlunos);
+            return true;
+        }
+    }
+}
